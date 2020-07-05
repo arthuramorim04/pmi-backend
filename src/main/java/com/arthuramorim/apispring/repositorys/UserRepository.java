b5670fbb354f8b0c4201e7abc19af894b025b6f6
arthuramorim04 <arthurpas67@gmail.com>
2020-07-05 12:59:14 -0300

    service layer, component registration

--- src/main/java/com/arthuramorim/apispring/repositorys/UserRepository.java
+++ src/main/java/com/arthuramorim/apispring/repositorys/UserRepository.java
@@ -3,6 +3,8 @@ package com.arthuramorim.apispring.repositorys;
 import com.arthuramorim.apispring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+// ja esta registrada como repositorio por extender a JpaRepository
 public interface UserRepository extends JpaRepository<User,Long> {
 
 }
---
+++ src/main/java/com/arthuramorim/apispring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.arthuramorim.apispring.services;
+
+import com.arthuramorim.apispring.entity.User;
+import com.arthuramorim.apispring.repositorys.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+// todos as chamadas utilizando o UserRepository devem ser feitas a partir dessa classe
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+
+}
