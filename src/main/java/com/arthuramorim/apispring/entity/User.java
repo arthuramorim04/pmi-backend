82292ca5f2135444f61bd4e91e6784a20fe7fe84
arthuramorim04 <arthurpas67@gmail.com>
2020-07-05 12:21:27 -0300

    atualizacao de dependencias

--- src/main/java/com/arthuramorim/apispring/entity/User.java
+++ src/main/java/com/arthuramorim/apispring/entity/User.java
@@ -1,17 +1,31 @@
 package com.arthuramorim.apispring.entity;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+//serializable serve para converter o objeto em uma base de bites
+
+@Entity
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String mail;
+
     private String phone;
+
     private String password;
 
     public User() {
