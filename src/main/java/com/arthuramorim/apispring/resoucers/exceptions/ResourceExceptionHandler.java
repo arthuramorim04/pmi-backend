99f4fafcd9e282c1a60ac3d9787aecb7223b721b
arthuramorim04 <arthurpas67@gmail.com>
2020-07-08 15:00:21 -0300

    adicionado tratamento basico na remoção de usuario do banco

--- src/main/java/com/arthuramorim/apispring/resoucers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/arthuramorim/apispring/resoucers/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.arthuramorim.apispring.resoucers.exceptions;
 
+import com.arthuramorim.apispring.services.exceptions.DatabaseException;
 import com.arthuramorim.apispring.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,4 +22,12 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+    //Trata erro de database
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandarError> database(DatabaseException erros, HttpServletRequest request){
+        String errorMsg = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandarError err = new StandarError(Instant.now(),status.value(),errorMsg,erros.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
 }
